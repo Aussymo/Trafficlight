17caf57eb7f7badb054ca7fb546e9a110237ee70
Aussymo <aussymo@gmail.com>
2022-04-13 23:52:49 +0000

    Created Repo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,50 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState("off");
+	let red = "bg-danger rounded-cicle";
+	let yellow = "bg-warning rounded-cicle";
+	let green = "bg-sucess rounded-cicle";
+	let off = "bg-dark rounded-cicle";
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div claName="cable">
+			<div className="lightcable"></div>
+			<div className="box">
+				<div className="light" onClick={() => setColor("red")}>
+					{color == "yellow" || color == "green" || color == "off" ? (
+						<div className={off}></div>
+					) : (
+						<div className={`glow ${red}`}></div>
+					)}
+				</div>
+				<div className="light" onClick={() => setColor("yellow")}>
+					{color == "red" || color == "green" || color == "off" ? (
+						<div className={off}></div>
+					) : (
+						<div className={`glower ${yellow}`}></div>
+					)}
+				</div>
+				<div className="light" onClick={() => setColor("green")}>
+					{color == "yellow" || color == "red" || color == "off" ? (
+						<div className={off}></div>
+					) : (
+						<div className={`glowerest ${green}`}></div>
+					)}
+				</div>
+				<div className="light"  onClick={() => setColor("green")}>
+					{color == "yellow" || color == "red" || color == "off" ? (
+						<div className={off}></div>
+					) : (
+						<div className={`glowerests ${green}`}></div>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
